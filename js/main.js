a7982748e17ccef75a28eee610831dd041b301ac
rouxarriere <chambard39@gmail.com>
2025-03-09 11:47:53 +0100

    MAJ : dev menu v2

--- js/main.js
+++ js/main.js
@@ -10,11 +10,14 @@ darkModeButton.addEventListener('click', () => {
 
 });
 
-const debug = document.getElementById('debugMode');
-var allElements = document.querySelectorAll(':not(nav):not(nav *)'); // Sélectionne tous les éléments de la page
-var menu = document.getElementsByTagName('nav');
+
+
 //debug mode
-debug.addEventListener('click', function() {
+const debug = document.getElementById('debugMode');
+const allElements = document.querySelectorAll(':not(nav):not(nav *)'); // Sélectionne tous les éléments de la page
+const menu = document.getElementsByTagName('nav');
+
+debug.addEventListener('click', () => {
  
   for (var i = 0; i < allElements.length; i++) {
     allElements[i].classList.toggle('debug'); // Ajoute ou supprime la classe 'debug'
@@ -25,8 +28,25 @@ debug.addEventListener('click', function() {
 
 });
 
+
+
+//Edit text
+const edit = document.getElementById('edit');
+const navElements = document.querySelectorAll('nav');
+let isEditable = false;
+
+        edit.addEventListener('click', () => {
+            isEditable = !isEditable;
+            document.body.contentEditable = isEditable;
+
+            navElements.forEach(navElement => {
+                navElement.contentEditable = false; // La navigation reste non éditable
+            });
+            edit.classList.toggle('clicked');
+        });
+
 const bouton = document.getElementById('coder');
-bouton.addEventListener("click", function() {
+bouton.addEventListener("click", () => {
   
   //window.open("https://docs.cless.dev", "_blank");
 });
