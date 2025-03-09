1b690591dada10011902950010263fe8b4cb4d10
rouxarriere <chambard39@gmail.com>
2025-03-09 11:28:17 +0100

    MAJ : dev menu v1

--- js/main.js
+++ js/main.js
@@ -2,44 +2,33 @@ const darkModeButton = document.getElementById('nuit');
 const body = document.body;
 
 const moon = document.querySelector('.fa-moon');
-const text = document.querySelector('.hidden');
-
-// Affichage du texte
-moon.addEventListener('mouseover', () => {
-  text.classList.remove('hidden');
-});
-moon.addEventListener('mouseout', () => {
-  text.classList.add('hidden');
-});
-
-
-
-
 // dark mode
 darkModeButton.addEventListener('click', () => {
   body.classList.toggle('nuit');
   darkModeButton.classList.toggle('wheat');
   darkModeButton.classList.toggle('fa-sun');
 
-  if (body.classList.contains('nuit')) {
-    text.textContent='Light Mode';
-  } else {
-    text.textContent='Dark Mode';
-  }
-
 });
 
-// Nouvelle page
-
-function nouvellePage(){
-  const element = document.querySelector('.container');
-  if (element) {
-    element.innerHTML = ''; // Supprime le contenu HTML du premier élément trouvé
+const debug = document.getElementById('debugMode');
+var allElements = document.querySelectorAll(':not(nav):not(nav *)'); // Sélectionne tous les éléments de la page
+var menu = document.getElementsByTagName('nav');
+//debug mode
+debug.addEventListener('click', function() {
+ 
+  for (var i = 0; i < allElements.length; i++) {
+    allElements[i].classList.toggle('debug'); // Ajoute ou supprime la classe 'debug'
   }
-};
+  debug.classList.toggle('clicked');
+  menu.classList.remove('debug');
+
+
+});
 
 const bouton = document.getElementById('coder');
 bouton.addEventListener("click", function() {
   
   //window.open("https://docs.cless.dev", "_blank");
 });
+
+
